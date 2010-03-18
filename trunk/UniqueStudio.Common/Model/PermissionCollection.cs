83f20a6a361b210a17c3a6c4c012ae8b487609c8
acenqiu@gmail.com <acenqiu@gmail.com@ccba9622-2aa0-11df-9df5-492cac88f9b2>
2010-03-18 13:32:11 +0000

    +)UniqueStudio.ExtraTools

--- trunk/UniqueStudio.Common/Model/PermissionCollection.cs
+++ trunk/UniqueStudio.Common/Model/PermissionCollection.cs
@@ -1,29 +1,36 @@
-﻿using System;
+﻿//=================================================================
+// 版权所有：版权所有(c) 2010，联创团队
+// 内容摘要：权限的集合。
+// 完成日期：2010年03月18日
+// 版本：v1.0 alpha
+// 作者：邱江毅
+//=================================================================
 using System.Collections.Generic;
-using System.Text;
 
 namespace UniqueStudio.Common.Model
 {
     /// <summary>
-    /// 权限的集合
+    /// 权限的集合。
     /// </summary>
-    public class PermissionCollection:List<PermissionInfo>
-	{
+    public class PermissionCollection : List<PermissionInfo>
+    {
         /// <summary>
-        /// 初始化<see cref="PermissionCollection"/>类的实例
+        /// 初始化<see cref="PermissionCollection"/>类的实例。
         /// </summary>
         public PermissionCollection()
-            :base()
+            : base()
         {
+
         }
 
         /// <summary>
-        /// 以集合容量初始化<see cref="PermissionCollection"/>类的实例
+        /// 以集合容量初始化<see cref="PermissionCollection"/>类的实例。
         /// </summary>
-        /// <param name="capacity">集合容量</param>
+        /// <param name="capacity">集合容量。</param>
         public PermissionCollection(int capacity)
             : base(capacity)
         {
+
         }
-	}
+    }
 }
